5cd83f7148f6c2640cada031cb94acd3502a098c
Sultim Tsyrendashiev <sultim.tsyrendashiev@gmail.com>
2021-03-11 00:18:11 +0300

    - dependency files map for GenerateShaders.py to prevent recompiling unnecessary shaders

--- Source/Shaders/GenerateShaders.py
+++ Source/Shaders/GenerateShaders.py
@@ -29,12 +29,38 @@ CACHE_FOLDER_PATH     = "Build/"
 CACHE_FILE_NAME       = "GenerateShadersCache.txt"
 EXTENSIONS            = [ ".comp", ".vert", "frag", ".rgen", ".rahit", ".rchit", ".rmiss" ]
 DEPENDENCY_EXTENSIONS = [ ".h", ".inl" ]
-DEPENDENCY_FOLDERS    = [ "../Generated/" ]
+DEPENDENCY_FOLDERS    = [ "", "../Generated/" ]
 DEPENDENCY_IGNORE     = [ "BlueNoiseFileNames.h", "ShaderCommonC.h", "ShaderCommonCFramebuf.h" ]
 
 
+CACHE_FILE_DEPENDENCY_MAP_SEPARATOR_LINE = "DEPENDENCY\n"
+
+
+MARKED_FILES = []
+def wereDependentModified(dependencyMap, cache, baseFile, firstTime=True):
+    global MARKED_FILES
+    if firstTime:
+        MARKED_FILES = []
+
+    for dpd in dependencyMap[baseFile]:
+        if dpd not in MARKED_FILES and os.path.exists(dpd):
+            if dpd not in cache:
+                return True
+                
+            dpdLastModifTime = int(pathlib.Path(dpd).stat().st_mtime)
+            dpdIsOutdated = dpd in cache and dpdLastModifTime != cache[dpd]
+            if dpdIsOutdated:
+                return True
+
+            MARKED_FILES.append(dpd)
+            if wereDependentModified(dependencyMap, cache, dpd, firstTime=False):
+                return True
+
+    return False
+
+
 def main():
-    if "--help" in sys.argv or "-help" in sys.argv:
+    if "--help" in sys.argv or "-help" in sys.argv or "-h" in sys.argv or "--h" in sys.argv:
         print("-rebuild  : clear cache and rebuild all shaders")
         print("-gencomm  : invoke GenerateShaderCommon.py script")
         print("-r        : same as \'-rebuild\"")
@@ -42,10 +68,13 @@ def main():
         return
 
     forceRebuild = False
-    if "-rebuild" in sys.argv or "-r" in sys.argv:
+    if "-rebuild" in sys.argv or "--rebuild" in sys.argv or "-r" in sys.argv or "--r" in sys.argv:
         forceRebuild = True
-    if "-gencomm" in sys.argv or "-g" in sys.argv:
+    elif "-gencomm" in sys.argv or "--gencomm" in sys.argv or "-g" in sys.argv or "--g" in sys.argv:
         subprocess.run(["python", "../Generated/GenerateShaderCommon.py", "--path", "../Generated/"])
+    elif len(sys.argv) > 1:
+        print("> Couldn't parse arguments")
+        return
 
     if not os.path.exists(CACHE_FOLDER_PATH):
         try:
@@ -62,22 +91,36 @@ def main():
             return
     with open(CACHE_FOLDER_PATH + CACHE_FILE_NAME, "r+") as cacheFile:
         cache = {}
+        dependencyMap = {}
 
         if not forceRebuild:
             try:
+                parsingDpdncy = False
                 for line in cacheFile:
-                    # filename + st_mtime
-                    nm = line.split()
-                    if len(nm) >= 2:
-                        cache[nm[0]] = int(nm[1])
+                    if line == CACHE_FILE_DEPENDENCY_MAP_SEPARATOR_LINE:
+                        parsingDpdncy = True
+                    else:
+                        words = line.split()
+                        if not parsingDpdncy and len(words) >= 2:
+                            # filename + st_mtime
+                            cache[words[0]] = int(words[1])
+
+                        if parsingDpdncy:
+                            if len(words) >= 2:
+                                # filename + (list of files it dependent on)
+                                dependencyMap[words[0]] = words[1:]
+                            else:
+                                dependencyMap[words[0]] = []
             except:
                 cache = {}
+                dependencyMap = {}
     
-    wereDependentModified = False
-    wasAnyShaderRebuilt = False
-    errorCount = 0
+    modifiedDependent = set()
 
-    for folder in [""] + DEPENDENCY_FOLDERS:
+    msgWasAnyShaderRebuilt = False
+    msgErrorCount = 0
+
+    for folder in DEPENDENCY_FOLDERS:
         if not forceRebuild:
             print("> Checking dependency files in " + ("current folder" if folder == "" else folder))
 
@@ -96,18 +139,26 @@ def main():
                 print("> File \"" + folder + filename + "\" has spaces in its path. Skipping.")
                 continue
 
-            lastModifTime = pathlib.Path(filename).stat().st_mtime
+            lastModifTime = int(pathlib.Path(filename).stat().st_mtime)
 
-            isOutdated = filename in cache and int(lastModifTime) != cache[filename]
+            isOutdated = filename in cache and lastModifTime != cache[filename]
 
             if filename not in cache or isOutdated:
-                # if dependency files were not cached or were modified, rebuild all
-                wereDependentModified = True
+                modifiedDependent.add(filename)
 
             cache[filename] = lastModifTime
 
-    if wereDependentModified and not forceRebuild:
-        print("> Dependency files were modified. Rebuilding all...")
+            if filename not in dependencyMap or isOutdated:
+                dependencyMap[filename] = []
+
+                with open(filename, "r") as dpd:
+                    for line in dpd:
+                        if line.startswith("#include"):
+                            dpdOnFile = line.split("\"")[1]
+                            dependencyMap[filename] += [dpdFolder + dpdOnFile for dpdFolder in DEPENDENCY_FOLDERS]
+
+    #if wereDependentModified and not forceRebuild:
+    #    print("> Dependency files were modified. Rebuilding all...")
     # print()
 
     for filename in os.listdir():
@@ -120,10 +171,19 @@ def main():
             print("> File \"" + filename + "\" has spaces in its name. Skipping.")
             continue
 
-        lastModifTime = pathlib.Path(filename).stat().st_mtime
-        isOutdated = filename in cache and int(lastModifTime) != cache[filename]
+        lastModifTime = int(pathlib.Path(filename).stat().st_mtime)
+        isOutdated = filename in cache and lastModifTime != cache[filename]
 
-        if filename not in cache or isOutdated or wereDependentModified:
+        if filename not in dependencyMap or isOutdated:
+            dependencyMap[filename] = []
+
+            with open(filename, "r") as dpd:
+                for line in dpd:
+                    if line.startswith("#include"):
+                        dpdOnFile = line.split("\"")[1]
+                        dependencyMap[filename] += [dpdFolder + dpdOnFile for dpdFolder in DEPENDENCY_FOLDERS]
+
+        if filename not in cache or isOutdated or wereDependentModified(dependencyMap, cache, filename):
             print("> Building " + filename)
 
             r = subprocess.run([
@@ -136,24 +196,28 @@ def main():
 
             if len(r.stdout) > 0:
                 print(r.stdout)
-                errorCount += 1
+                msgErrorCount += 1
                 if filename in cache:
                     del cache[filename]
             else:
                 cache[filename] = lastModifTime
 
-            wasAnyShaderRebuilt = True
+            msgWasAnyShaderRebuilt = True
 
     with open(CACHE_FOLDER_PATH + CACHE_FILE_NAME, "w") as cacheFile:
         for name, tm in cache.items():
-            cacheFile.write(name + " " + str(int(tm)) + "\n")
+            cacheFile.write(name + " " + str(tm) + "\n")
+        cacheFile.write(CACHE_FILE_DEPENDENCY_MAP_SEPARATOR_LINE)
+        for name, arr in dependencyMap.items():
+            arrStr = " ".join(arr)
+            cacheFile.write(name + " " + arrStr + "\n")
 
     #if wereDependentModified:
     #    print()
 
-    if errorCount > 0:
-        print("> " + str(errorCount) + (" shader build failed." if errorCount == 1 else " shader builds failed."))
-    elif not wasAnyShaderRebuilt:
+    if msgErrorCount > 0:
+        print("> " + str(msgErrorCount) + (" shader build failed." if msgErrorCount == 1 else " shader builds failed."))
+    elif not msgWasAnyShaderRebuilt:
         print("> Everything is up-to-date.")
 
 
