a04baaaf096e86b57da5cbef904bb3b778d4ad85
Sultim Tsyrendashiev <sultim.tsyrendashiev@gmail.com>
2022-07-02 11:09:30 +0100

    - barriers for FSR2 resources - fixed output image not being FFX_RESOURCE_STATE_UNORDERED_ACCESS

--- Source/FSR2.cpp
+++ Source/FSR2.cpp
@@ -84,15 +84,67 @@ void RTGL1::FSR2::OnFramebuffersSizeChange(const ResolutionState &resolutionStat
 
 namespace 
 {
+    constexpr RTGL1::FramebufferImageIndex OUTPUT_IMAGE_INDEX = RTGL1::FB_IMAGE_INDEX_UPSCALED_PONG;
+
     FfxResource ToFSRResource(
         RTGL1::FramebufferImageIndex fbImage, uint32_t frameIndex,
         FfxFsr2Context *pCtx,
-        const std::shared_ptr<RTGL1::Framebuffers> &framebuffers,
+        const RTGL1::Framebuffers &framebuffers,
         const RTGL1::ResolutionState &resolutionState)
     {
-        auto [image, view, format, sz] = framebuffers->GetImageHandles(fbImage, frameIndex, resolutionState);
+        auto [image, view, format, sz] = framebuffers.GetImageHandles(fbImage, frameIndex, resolutionState);
 
-        return ffxGetTextureResourceVK(pCtx, image, view, sz.width, sz.height, format);
+        return ffxGetTextureResourceVK(
+            pCtx, image, view, sz.width, sz.height, format, nullptr,
+            fbImage == OUTPUT_IMAGE_INDEX ? FFX_RESOURCE_STATE_UNORDERED_ACCESS : FFX_RESOURCE_STATE_COMPUTE_READ);
+    }
+
+    template<size_t N>
+    void InsertBarriers(
+        VkCommandBuffer cmd, uint32_t frameIndex, RTGL1::Framebuffers &framebuffers, 
+        const RTGL1::FramebufferImageIndex (&inputsAndOutput)[N],
+        bool isBackwards)
+    {
+        assert(std::find(std::begin(inputsAndOutput), std::end(inputsAndOutput), OUTPUT_IMAGE_INDEX) != std::end(inputsAndOutput));
+
+        VkImageMemoryBarrier2 barriers[N];
+
+        for (size_t i = 0; i < N; i++)
+        {
+            auto &b = barriers[i];
+            b = {};
+
+            b.sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER_2;
+            b.srcStageMask = VK_PIPELINE_STAGE_2_RAY_TRACING_SHADER_BIT_KHR | VK_PIPELINE_STAGE_2_COMPUTE_SHADER_BIT;
+            b.srcAccessMask = VK_ACCESS_2_SHADER_WRITE_BIT | VK_ACCESS_2_SHADER_READ_BIT;
+            b.dstStageMask = VK_PIPELINE_STAGE_2_COMPUTE_SHADER_BIT;
+            b.dstAccessMask = inputsAndOutput[i] == OUTPUT_IMAGE_INDEX ? VK_ACCESS_2_SHADER_WRITE_BIT : VK_ACCESS_2_SHADER_READ_BIT;
+            b.oldLayout = VK_IMAGE_LAYOUT_GENERAL;
+            b.newLayout = inputsAndOutput[i] == OUTPUT_IMAGE_INDEX ? VK_IMAGE_LAYOUT_GENERAL : VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
+            b.srcQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
+            b.dstQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
+            b.image = framebuffers.GetImage(inputsAndOutput[i], frameIndex);
+            b.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+            b.subresourceRange.baseMipLevel = 0;
+            b.subresourceRange.levelCount = 1;
+            b.subresourceRange.baseArrayLayer = 0;
+            b.subresourceRange.layerCount = 1;
+
+            if (isBackwards)
+            {
+                std::swap(b.srcStageMask, b.dstStageMask);
+                std::swap(b.srcAccessMask, b.dstAccessMask);
+                std::swap(b.oldLayout, b.newLayout);
+                std::swap(b.srcQueueFamilyIndex, b.dstQueueFamilyIndex);
+            }
+        }
+
+        VkDependencyInfoKHR dependencyInfo = {};
+        dependencyInfo.sType = VK_STRUCTURE_TYPE_DEPENDENCY_INFO_KHR;
+        dependencyInfo.imageMemoryBarrierCount = static_cast<uint32_t>(std::size(barriers));
+        dependencyInfo.pImageMemoryBarriers = barriers;
+
+        RTGL1::svkCmdPipelineBarrier2KHR(cmd, &dependencyInfo);
     }
 }
 
@@ -107,21 +159,29 @@ RTGL1::FramebufferImageIndex RTGL1::FSR2::Apply(
     float fovVerticalRad)
 {
     assert(nearPlane > 0.0f && nearPlane < farPlane);
+    
+    using FI = RTGL1::FramebufferImageIndex;
 
-    typedef FramebufferImageIndex FI;
-    const FI outputImage = FI::FB_IMAGE_INDEX_UPSCALED_PONG;
+    FI rs[] =
+    {
+        FI::FB_IMAGE_INDEX_FINAL,
+        FI::FB_IMAGE_INDEX_DEPTH_DLSS,
+        FI::FB_IMAGE_INDEX_MOTION_DLSS,
+        OUTPUT_IMAGE_INDEX
+    };
+    InsertBarriers(cmd, frameIndex, *framebuffers, rs, false);
 
     FfxFsr2Context *pCtx = &context->value();
 
     FfxFsr2DispatchDescription info = {};
     info.commandList = ffxGetCommandListVK(cmd);
-    info.color = ToFSRResource(FI::FB_IMAGE_INDEX_FINAL, frameIndex, pCtx, framebuffers, renderResolution.GetResolutionState());
-    info.depth = ToFSRResource(FI::FB_IMAGE_INDEX_DEPTH_DLSS, frameIndex, pCtx, framebuffers, renderResolution.GetResolutionState());
-    info.motionVectors = ToFSRResource(FI::FB_IMAGE_INDEX_MOTION_DLSS, frameIndex, pCtx, framebuffers, renderResolution.GetResolutionState());
+    info.color = ToFSRResource(FI::FB_IMAGE_INDEX_FINAL, frameIndex, pCtx, *framebuffers, renderResolution.GetResolutionState());
+    info.depth = ToFSRResource(FI::FB_IMAGE_INDEX_DEPTH_DLSS, frameIndex, pCtx, *framebuffers, renderResolution.GetResolutionState());
+    info.motionVectors = ToFSRResource(FI::FB_IMAGE_INDEX_MOTION_DLSS, frameIndex, pCtx, *framebuffers, renderResolution.GetResolutionState());
     info.exposure = {};
     info.reactive = {};
     info.transparencyAndComposition = {};
-    info.output = ToFSRResource(outputImage, frameIndex, pCtx, framebuffers, renderResolution.GetResolutionState());
+    info.output = ToFSRResource(OUTPUT_IMAGE_INDEX, frameIndex, pCtx, *framebuffers, renderResolution.GetResolutionState());
     info.jitterOffset.x = jitterOffset.data[0];
     info.jitterOffset.y = jitterOffset.data[1];
     info.motionVectorScale.x = static_cast<float>(renderResolution.GetResolutionState().renderWidth);
@@ -140,7 +200,9 @@ RTGL1::FramebufferImageIndex RTGL1::FSR2::Apply(
     FfxErrorCode r = ffxFsr2ContextDispatch(pCtx, &info);
     CheckError(r);
 
-    return outputImage;
+    InsertBarriers(cmd, frameIndex, *framebuffers, rs, true);
+
+    return OUTPUT_IMAGE_INDEX;
 }
 
 RgFloat2D RTGL1::FSR2::GetJitter(const ResolutionState& resolutionState, uint32_t frameId)
